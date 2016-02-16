f710df0a06ef1ed064ead0326fa0d1957ffadccc
James Nielson and Kyle Langley <student@epicodus.com>
2016-02-16 08:22:44 -0800

    initial commit

---
+++ js/time-interface.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  tickTime( );
+  setInterval( tickTime, 1000 );
+  function tickTime( )
+  {
+    var time = moment( );
+    var hours = time._d.getHours( ).toString( );
+    var minutes = time._d.getMinutes( ).toString( );
+    var seconds = time._d.getSeconds( ).toString( );
+
+    if( hours < 10 )
+    {
+      var start = "0" + hours;
+      hours = start;
+    }
+
+    if( minutes < 10 )
+    {
+      var start = "0" + minutes;
+      minutes = start;
+    }
+
+    if( seconds < 10 )
+    {
+      var start = "0" + seconds;
+      seconds = start;
+    }
+    var printOut = hours + ":" + minutes + ":" + seconds;
+    $("#time").text(printOut);
+  }
+});
