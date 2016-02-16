aea8da4d82de4031e57d9352717e80054c63f233
James Nielson and Kyle Langley <altck8@gmail.com>
2016-02-16 09:00:09 -0800

    add build gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var browserify  = require('browserify');
+var utilities   = require('gulp-util');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var source      = require('vinyl-source-stream');
+var gulp        = require('gulp');
+var del         = require('del');
+
+var buildProduction = utilities.env.production;
 
 gulp.task("time", function( ) {
   console.log("time works");
@@ -32,4 +36,16 @@ gulp.task("minifyScripts", ["jsBrowserify"], function(){
   return gulp.src("./build/js/app.js")
     .pipe(uglify())
     .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
 });
